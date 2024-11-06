ca293686868ec03a9dbc09b437e773cd34b1a1cd
Bryan Song Ching Hang <bryansong25@gmail.com>
2024-11-06 23:50:34 +0800

    Add files via upload

---
+++ E-commerce-project/script.js
@@ -0,0 +1,19 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+const pro = document.getElementsByClassName('pro-container');
+
+if(bar) {
+  bar.addEventListener('click',()=>{
+    nav.classList.add('active');
+  })
+}
+
+if(close) {
+  close.addEventListener('click',()=>{
+    nav.classList.remove('active');
+  })
+}
+
+
+
